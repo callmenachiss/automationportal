b09e599b3b9a16c26ac09fc345f5d510b47cc0fa
Nachiyappan Jeya <nachiyappan7777@gmail.com>
2025-10-16 10:30:44 +0530

    remove unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-import LoginPage from './components/LoginPage'
-import DashboardPage from './components/DashboardPage'
 import LandingPage from './components/LandingPage'
-import SignUpPage from './components/SignUpPage';
 
 
 function App() {
@@ -13,9 +10,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
      
       </Routes>
     </BrowserRouter>
